cd01e95bdb7e0df29f788ee01911b949a67dcf36
lcsvinhas <l.vinhas.lv@gmail.com>
2025-12-15 19:10:30 -0300

    Feat: criação dos models

---
+++ Harmonia.API/Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+
+namespace Harmonia.API.Models;
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public ICollection<Instrumento>? Instrumentos { get; set; } = new Collection<Instrumento>();
+}
---
+++ Harmonia.API/Models/Instrumento.cs
@@ -0,0 +1,15 @@
+﻿namespace Harmonia.API.Models;
+
+public class Instrumento
+{
+    public int InstrumentoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Marca { get; set; }
+    public string? Modelo { get; set; }
+    public string? Descricao { get; set; }
+    public string? Cor { get; set; }
+    public string? Material { get; set; }
+    public int AnoFabricacao { get; set; }
+    public int CategoriaId { get; set; }
+    public Categoria? Categoria { get; set; }
+}
